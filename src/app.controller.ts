f1752169419cee7c758f1ce9e087cf5be5d17471
Maximilian <mpinderwhite@gmail.com>
2025-02-02 20:40:16 +0100

    fix: backend routes to /api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthGuard } from '@nestjs/passport';
 
-@Controller()
+@Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
