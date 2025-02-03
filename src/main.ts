f61baceb78093075757b1ffdb8e0f4b4fe70872c
Maximilian <61150071+MaxPW777@users.noreply.github.com>
2025-02-03 07:19:35 +0100

    fix: set api as global prefix

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import * as dotenv from 'dotenv';
 async function bootstrap() {
   dotenv.configDotenv();
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
