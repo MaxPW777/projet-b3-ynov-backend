8850f1eaf4c802330eb9e726467ebee25bc0a4b3
Maximilian <mpinderwhite@gmail.com>
2025-02-13 20:33:39 +0100

    fix: fixed jwktopem

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
 import { CognitoStrategy } from './auth/cognito.strategy';
 import { AuthController } from './auth/auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Constants } from './constants';
+import { AuthModule } from './auth/auth.module';
+import { PreferenceModule } from './preferences/preference.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: Constants.DATABASE_HOST,
+      port: 5432,
+      username: Constants.DATABASE_USER,
+      password: Constants.DATABASE_PASSWORD,
+      entities: ['/**/*.entity.js'],
+      database: 'postgres',
+      synchronize: true,
+    }),
+    AuthModule,
+    PreferenceModule,
+  ],
   controllers: [AppController, AuthController],
   providers: [AppService, CognitoStrategy],
 })
--- src/auth/cognito.strategy.ts
+++ src/auth/cognito.strategy.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import * as jwkToPem from 'jwk-to-pem';
+import jwkToPem from 'jwk-to-pem';
 import * as jwt from 'jsonwebtoken';
 import axios from 'axios';
 
