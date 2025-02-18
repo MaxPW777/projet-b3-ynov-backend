d4129e8efbb2f2e009c13130e9ec05f0e3985614
Maximilian <mpinderwhite@gmail.com>
2025-02-18 18:34:28 +0100

    feat: added validator

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   dotenv.configDotenv();
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: (errors): Error => {
+        const error = new BadRequestException(errors);
+        return error;
+      },
+    }),
+  );
+
   app.setGlobalPrefix('api');
   await app.listen(8080, '0.0.0.0');
 }
